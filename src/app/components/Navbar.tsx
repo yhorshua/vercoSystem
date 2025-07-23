7d1a30837abf16c33a8e88b7f103dc5601e03209
jose nieva <joseluisnievavillogas@gmail.com>
2025-07-23 12:47:35 -0500

    fix: navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,21 +2,39 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link'; // Usamos Link para la redirección en Next.js
 
 const Navbar = ({ username, userArea }: { username: string, userArea: string }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen); // Alterna el estado del menú
+    setIsMenuOpen(!isMenuOpen); // Alternar el estado del menú
   };
 
   return (
     <nav className="navbar">
+      {/* Logo en el lado izquierdo */}
+      <div className="navbar-logo">
+        <Image
+          src="/img/verco_logo.png" // Ruta de la imagen del logo
+          alt="Logo Empresa"
+          width={100} // Ajusta el tamaño del logo según sea necesario
+          height={40}
+        />
+      </div>
+
       {/* Botón de menú hamburguesa alineado a la izquierda */}
       <div className="hamburger" onClick={toggleMenu}>
         <span className="bar"></span>
         <span className="bar"></span>
         <span className="bar"></span>
+      </div>
+
+      {/* Contenedor de enlaces centrados */}
+      <div className="navbar-center">
+        <Link href="/register-pedido" className="navbar-link">Registrar Pedido</Link>
+        <Link href="/lista-pedidos" className="navbar-link">Lista de Pedidos</Link>
+        <Link href="/stock" className="navbar-link">Stock</Link>
       </div>
 
       {/* Contenedor del usuario, se muestra cuando se abre el menú */}
